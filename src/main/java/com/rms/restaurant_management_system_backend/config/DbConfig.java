509a3455a8da32a53fac142dd1bfc156a8486def
Datla Rohit Varma <visaal1582892@gmail.com>
2025-08-20 16:12:50 +0530

    created a seperate file for mysql queries and for queries which we use in daos

--- src/main/java/com/rms/restaurant_management_system_backend/config/DbConfig.java
+++ src/main/java/com/rms/restaurant_management_system_backend/config/DbConfig.java
@@ -1,5 +1,19 @@
 package com.rms.restaurant_management_system_backend.config;
 
-public class DbConfig {
+import javax.sql.DataSource;
 
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+public class DbConfig {
+	@Bean
+    public DataSource dataSource() {
+        return DataSourceBuilder.create().build();
+    }
+
+    @Bean
+    public JdbcTemplate jdbcTemplate(DataSource dataSource) {
+        return new JdbcTemplate(dataSource);
+    }
 }
