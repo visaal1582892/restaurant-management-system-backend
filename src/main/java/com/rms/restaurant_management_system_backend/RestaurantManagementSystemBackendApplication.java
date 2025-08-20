f4d523146845f61455f3a1d0b4e8daa950b951da
Datla Rohit Varma <visaal1582892@gmail.com>
2025-08-20 11:21:02 +0530

    Initial Commit

---
+++ src/main/java/com/rms/restaurant_management_system_backend/RestaurantManagementSystemBackendApplication.java
@@ -0,0 +1,13 @@
+package com.rms.restaurant_management_system_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantManagementSystemBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantManagementSystemBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rms/restaurant_management_system_backend/RestaurantManagementSystemBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rms.restaurant_management_system_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantManagementSystemBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
