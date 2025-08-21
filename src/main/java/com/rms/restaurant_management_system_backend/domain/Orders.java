834325c407312dccf9b5916bb6ff23e68fcb28ff
Allegapally sahithi <allegapallysahithi@gmail.com>
2025-08-21 15:45:28 +0530

    made some changes

--- src/main/java/com/rms/restaurant_management_system_backend/domain/Orders.java
+++ src/main/java/com/rms/restaurant_management_system_backend/domain/Orders.java
@@ -5,6 +5,7 @@ import java.time.LocalDate;
 import com.rms.restaurant_management_system_backend.constant.OrderStatus;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -17,26 +18,25 @@ public class Orders {
 
 	private int orderId;
 
-	@NotBlank(message = "Customer id is required")
+//	@NotNull(message = "Customer id is required")
 	private int customerId;
 
-	@NotBlank(message = "Staff id is required")
+//	@NotNull(message = "Staff id is required")
 	private int waiterId;
 
-	@NotBlank(message = "Order Date is required")
+//	@NotBlank(message = "Order Date is required")
 	private LocalDate orderDate;
 
-	@NotBlank(message = "Amount is required")
-	@Positive(message = "Amount must be a positive number")
+//	@NotNull(message = "Amount is required")
+//	@Positive(message = "Amount must be a positive number")
 	private double amount;
 
-	@NotBlank(message = "Status is required")
+//	@NotBlank(message = "Status is required")
 	private OrderStatus status;
 
-	public Orders(int customerId, int waiterId, double amount) {
+	public Orders(int customerId, int waiterId) {
 		this.customerId = customerId;
 		this.waiterId = waiterId;
-		this.amount = amount;
 	}
 
 }
