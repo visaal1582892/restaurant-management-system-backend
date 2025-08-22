b82be7359921c1d5d7d1315288853c830023a951
Allegapally sahithi <allegapallysahithi@gmail.com>
2025-08-22 16:53:40 +0530

    updated orders service

--- src/main/java/com/rms/restaurant_management_system_backend/service/implementation/OrdersServiceImpl.java
+++ src/main/java/com/rms/restaurant_management_system_backend/service/implementation/OrdersServiceImpl.java
@@ -4,48 +4,67 @@ import java.time.LocalDate;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.rms.restaurant_management_system_backend.constant.OrderStatus;
 import com.rms.restaurant_management_system_backend.dao.OrderDetailsDao;
 import com.rms.restaurant_management_system_backend.dao.OrdersDao;
+import com.rms.restaurant_management_system_backend.dao.implementation.CustomerDaoImpl;
+import com.rms.restaurant_management_system_backend.domain.Customer;
 import com.rms.restaurant_management_system_backend.domain.OrderDetails;
 import com.rms.restaurant_management_system_backend.domain.Orders;
 import com.rms.restaurant_management_system_backend.exception.DatabaseOperationException;
 import com.rms.restaurant_management_system_backend.exception.InvalidDataException;
 import com.rms.restaurant_management_system_backend.exception.ResourceNotFoundException;
+import com.rms.restaurant_management_system_backend.service.CustomerService;
 import com.rms.restaurant_management_system_backend.service.OrdersService;
+import com.rms.restaurant_management_system_backend.service.WaitersService;
 
 @Service
 public class OrdersServiceImpl implements OrdersService {
 
 	private final OrdersDao ordersDao;
+	private final CustomerService customerService;
 	private final OrderDetailsDao orderDetailsDao;
+	private final WaitersService waitersService;
 
-	public OrdersServiceImpl(OrdersDao ordersDao, OrderDetailsDao orderDetailsDao) {
+	public OrdersServiceImpl(OrdersDao ordersDao, OrderDetailsDao orderDetailsDao, WaitersService waitersService,
+			CustomerService customerService) {
 		this.ordersDao = ordersDao;
-		this.orderDetailsDao=orderDetailsDao;
+		this.customerService = customerService;
+		this.orderDetailsDao = orderDetailsDao;
+		this.waitersService = waitersService;
 	}
 
 	@Override
-	public void addOrder(Orders order) {
-		// order - customer id, waiter id
-		// check customer id, waiter id
-
+	public int addOrder(String name, String phone, int waiterId) {
+		int customerId = 0;
+		try {
+			customerId = new CustomerDaoImpl().getCustomerIdByNumber(phone);
+		} catch (EmptyResultDataAccessException ex) {
+			customerId = customerService.addCustomer(new Customer(name, phone));
+		}
+		waitersService.updateWaiterAvailability(waiterId);
+		Orders order = new Orders(customerId, waiterId);
 		order.setOrderDate(LocalDate.now());
 		order.setStatus(OrderStatus.PENDING);
 		int rows = ordersDao.addOrder(order);
 		if (rows != 1) {
 			throw new DatabaseOperationException("Failed to create new order");
 		}
+		int id = ordersDao.getOrderId(order);
+		if (id == 0) {
+			throw new DatabaseOperationException("Failed to get Order Id");
+		}
+		return id;
 	}
 
 	@Override
 	public void updateStatus(int id, OrderStatus status) {
 		Orders order = ordersDao.getOrderById(id);
-		if(order == null) {
-			throw new InvalidDataException(
-					"Order with ID " + order.getOrderId() + " does not exist");
+		if (order == null) {
+			throw new InvalidDataException("Order with ID " + order.getOrderId() + " does not exist");
 		}
 		if (!order.getStatus().equals(OrderStatus.PENDING)) {
 			throw new InvalidDataException(
@@ -55,6 +74,7 @@ public class OrdersServiceImpl implements OrdersService {
 		if (rows != 1) {
 			throw new DatabaseOperationException("Failed to update status with ID " + order.getOrderId());
 		}
+		waitersService.updateWaiterAvailability(order.getWaiterId());
 	}
 
 	@Override
@@ -108,13 +128,12 @@ public class OrdersServiceImpl implements OrdersService {
 		return orders;
 	}
 
-	
 	public double calculateTotalAmount(int id) {
 		double amount = 0;
-		List<OrderDetails> details = orderDetailsDao.selectAllOrderDetails().stream()
-				.filter(d -> d.getOrderId() == id).collect(Collectors.toList());
+		List<OrderDetails> details = orderDetailsDao.selectAllOrderDetails().stream().filter(d -> d.getOrderId() == id)
+				.collect(Collectors.toList());
 		for (OrderDetails detail : details) {
-			amount += detail.getQuantity()*detail.getPrice();
+			amount += detail.getQuantity() * detail.getPrice();
 		}
 		return amount;
 	}
