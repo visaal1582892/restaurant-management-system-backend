0a4d41bf682cbe41604e0fbfc216c69054d3cd7c
tarun sasapu <tarun.sasapu@example.com>
2025-08-22 15:33:37 +0530

    made changes

--- src/main/java/com/rms/restaurant_management_system_backend/controller/WaitersController.java
+++ src/main/java/com/rms/restaurant_management_system_backend/controller/WaitersController.java
@@ -4,22 +4,24 @@ import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.rms.restaurant_management_system_backend.domain.Waiters;
 import com.rms.restaurant_management_system_backend.service.WaitersService;
 
+@RestController
 @RequestMapping("/api/staff/waiters")
 public class WaitersController {
-	
+
 	private WaitersService waitersService;
-	
+
 	public WaitersController(WaitersService waitersService) {
-		this.waitersService=waitersService;
+		this.waitersService = waitersService;
 	}
-	
+
 	@GetMapping("/available")
-	public List<Waiters> getAllAvailableWaiters(){
-		List<Waiters> availableWaiters=waitersService.selectAvailableWaiters();
+	public List<Waiters> getAllAvailableWaiters() {
+		List<Waiters> availableWaiters = waitersService.selectAvailableWaiters();
 		return availableWaiters;
 	}
 }
